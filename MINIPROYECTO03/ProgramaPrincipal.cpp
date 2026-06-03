a76027aa89e87fc64d940b7760610e35ae691fbc
jhoelmachicadoF1 <jhoel.machicado@ucb.edu.bo>
2026-06-03 14:33:56 -0400

    Add product management menu to main program

--- MINIPROYECTO03/ProgramaPrincipal.cpp
+++ MINIPROYECTO03/ProgramaPrincipal.cpp
@@ -1,10 +1,59 @@
-//PROGRAMA PRINCIPAL
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main ()
+struct structProducto 
 {
+    int codigo;
+    char nombre[30];
+    int cantidadinicial;
+    double preciounitario;
+};
+
+void mostrarMenu() 
+{
+    cout << " SISTEMA FARMACIAS CHAVEZ\n";
+    cout << "1. Adicionar producto\n";
+    cout << "2. Procesar ventas\n";
+    cout << "3. Lista productos\n";
+    cout << "4. Salir\n";
+    cout << "Elija una opcion: ";
+}
+
+int main() 
+{
+    int opcion;
+
+    do 
+    {
+        mostrarMenu();
+        cin >> opcion;
+
+        if (opcion == 1) 
+        {
+            cout << "\nAqui se adicionara un producto";
+        } 
+        else if (opcion == 2) 
+        {
+            cout << "\nAqui se procesaran las ventas";
+        } 
+        else if (opcion == 3) 
+        {
+            cout << "\nAqui se listaran los productos";
+        } 
+        else if (opcion == 4) 
+        {
+            cout << "\nSaliendo del programa";
+        } 
+        else 
+        {
+            cout << "\nOpcion incorrecta";
+        }
+
+    } 
+        while (opcion != 4);
 
     return 0;
 }
