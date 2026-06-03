969d7c6b8360ccaa1e5021dcde56a015b903c58c
jhoelmachicadoF1 <jhoel.machicado@ucb.edu.bo>
2026-06-03 17:57:53 -0400

    Update LibJhoel.h

--- MINIPROYECTO03/LibJhoel.h
+++ MINIPROYECTO03/LibJhoel.h
@@ -5,6 +5,17 @@
 #include <iomanip>
 
 
+using namespace std;
+
+struct structProducto 
+{
+    int codigo;
+    char nombre[30];
+    int cantidadInicial;
+    double precioUnitario;
+};
+
+
 bool codigoexiste(int codigobuscado) 
 {
     ifstream archivo("PRODUCTOS.BIN", ios::binary);
